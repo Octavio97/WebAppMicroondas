459fcabf5a584585092f9cf57aaff3b20b3761a2
Octavio <octavio.gastelum@tectijuana.edu.mx>
2020-08-28 14:50:48 -0700

    se agregaron los modulos de login y admin, modificacion al inicio

--- FrontEnd/WebApp/src/app/app.module.ts
+++ FrontEnd/WebApp/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: '', component: InicioComponent }
+      { path: '', component: InicioComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'admin', component: AdminComponent }
     ])
   ],
   providers: [],
